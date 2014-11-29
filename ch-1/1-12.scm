d5bcc5027de7b690c7ccb5d7abeaa2526f452f3f
russphelan <russ.phelan@gmail.com>
2014-11-28 23:27:16 -0500

    minor changes, cond to if, etc

--- ch-1/1-12.scm
+++ ch-1/1-12.scm
@@ -1,6 +1,5 @@
 (define (pascal rows cols)
-
-        ((or (= cols 0)(= cols rows)) 1)
-        (else (+ (pascal (- rows 1)(- cols 1))
-                 (pascal (- rows 1) cols)))))
-      
+  (if (or (= cols 0) (= cols rows))
+      1
+      (+ (pascal (- rows 1) (- cols 1))
+	 (pascal (- rows 1) cols))))
